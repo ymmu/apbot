6a9c78aca34b3f5ed80e6f7ab7c7466d84e2e12b
lucca <myohyun@naver.com>
2022-01-28 01:42:58 +0900

    210128fri - edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-${app_home:=$(pwd)}
+app_name="apbot"
+app_home=$(pwd)
+app_env=$app_home/env
+
 
 # make a folder logs
-mkdir $app_home"/logs"
-
-$app_env=$app_home"/env"
-
-# make a virtual env
-if [ -e $app_env"/bin/activate" ]; then
-        python3 -m virtualenv env
+if [ ! -e "${app_env}/logs" ]; then
+        mkdir $app_env/logs
 fi
 
-source $app_env"/bin/activate"
+# make a virtual env
+if [ ! -e "${app_env}/bin/activate" ]; then
+        virtualenv env
+fi
+
+source $app_env/bin/activate
 
 # install libs
 pip3 install -r requirements.txt
 
+ln -s $app_home/elk/logstash/pipeline/$app_name.conf /data/lucca/elk/logstash/pipeline/$app_name.conf
+cat $app_home/elk/logstash/config/pipelines.yml >> /data/lucca/elk/logstash/config/pipelines.yml
